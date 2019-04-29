ebc35bfa4f4861014d1e077bedbcc41fdae0fd6e
Dan Fedick <dan@zaius.com>
2019-04-29 09:54:34 -0400

    Swapping out Vault Tokens in script

--- mysql-dynamic-secrets/scripts/5_create_policy.sh
+++ mysql-dynamic-secrets/scripts/5_create_policy.sh
@@ -3,20 +3,19 @@
 export VAULT_TOKEN=$(cat ~/.vault-token)
 export VAULT_ADDR=http://127.0.0.1:8200
 
-export MYSQL_ROOT_PASSWORD=$(vault kv get -field=MYSQL_ROOT_PASSWORD secret/mysqldb)
-export MYSQL_DATABASE=$(vault kv get -field=MYSQL_DATABASE secret/mysqldb)
-
-export MYSQL_USER=the_fed
-export MYSQL_PASSWORD=$(vault kv get -field=MYSQL_PASSWORD secret/mysqldb)
+export MYSQL_USER=$(vault kv get -field=MYSQL_USER secret/mysqldb)
 export MYSQL_ROLE=mysql_admin_role
 
 
 
-cat << EOF > ./policies/${MYSQL_USER}.hcl
+cat << EOF > ../policies/${MYSQL_USER}.hcl
 path "sys/*"                              { policy = "deny" }
-path "sys/policy/dfedick"                 { capabilities = ["read"] }
+path "sys/policies/acl/${MYSQL_USER}"     { capabilities = ["read"] }
 path "database/creds/${MYSQL_ROLE}"       { capabilities =  ["create", "read", "update", "delete", "list", "sudo"] }
 EOF
 
 vault policy write ${MYSQL_USER} ../policies/${MYSQL_USER}.hcl
-vault token create -policy=${MYSQL_USER}
+token=$(vault token create -format=json -policy="${MYSQL_USER}" -display-name="${MYSQL_USER}" | jq -r .auth.client_token)
+
+cp ~/.vault-token ~/.vault-token-root
+echo ${token} > ~/.vault-token 
